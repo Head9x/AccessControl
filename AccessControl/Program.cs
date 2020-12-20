e67a173e0a9b147da6099b70cda98ec1b65b1c10
Head9x <git@caf-tranberg.dk>
2020-12-21 00:40:47 +0100

    1st release of a working program
    Made a simple program to read/write console statements about cards and parse info from my rfid/nfc reader

---
+++ AccessControl/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Define access giving card
+            string accesscard = "1606860297";
+
+            // Present card to reader
+            Console.WriteLine("Present card:");
+
+            // Create a string storing card info
+            string presentedCard = Console.ReadLine();
+
+            // Check compare string with card number defined earlier
+            if (presentedCard == accesscard)
+            {
+                Console.WriteLine("Card Accepted");
+
+            }
+            else
+            {
+                Console.WriteLine("Card Denied");
+
+            };
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # RFID/NFC door access
  Gives access to certain users with predefined cards
+
+Repository made for version control/keep track of what I made when and making sure that I can always go back a version, in case I mess up.
